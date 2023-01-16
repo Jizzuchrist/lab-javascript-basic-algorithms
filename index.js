88312b8370fa82ee0ead85ee1b0eb2d904357eda
Jizzus <miguel.angelo.jesus@hotmail.com>
2023-01-16 17:55:59 +0000

    iteration 2

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 // Iteration 1: Names and Input
 console.log("I'm ready!");
 
-// Iteration 2: Conditionals
+let hacker1 = "Erika"
+let hacker2 = "Miguel"
 
+console.log(`The driver's name is ${hacker1}`);
+console.log(`The navigator's name is ${hacker2}`);
+// Iteration 2: Conditionals
+let size1 = hacker1.length;
+let size2 = hacker2.length;
+
+if (size1 > size2){
+    console.log(`The driver has the longest name, it has ${size1} characters.`)
+} else if (size1 < size2){
+    console.log(`It seems that the navigator has the longest name, it has ${size2}`)
+} else {
+    console.log(`Wow, you both have equally long names, ${size1}`)
+}
 
 // Iteration 3: Loops
+toUpperCase()
